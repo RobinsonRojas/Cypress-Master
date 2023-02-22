0ab338baf01bfaab17c982accd3fb8ae3cd5672b
Robinson Arley Rojas Novoa <totorox123@gmail.com>
2023-02-22 17:13:57 -0500

    Ajuste URL base

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,7 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://dzfebp6b12rzi.cloudfront.net/login')
+    cy.visit('https://dzfebp6b12rzi.cloudfront.net/login', {
+      failOnStatusCode: false,
+    })
   })
 })
